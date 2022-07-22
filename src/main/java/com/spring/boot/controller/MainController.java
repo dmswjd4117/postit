fda0906a1628179d7e5da3f05efa142cc6748ff2
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-07-22 17:07:46 +0900

    Initial commit

---
+++ src/main/java/com/spring/boot/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(){
+        return "main";
+    }
+
+}
