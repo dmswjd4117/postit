b951ff8c69e7e3d4133042c5634d62ef686dd0e7
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-08-18 14:26:56 +0900

    test: jenkins 파이프라인 테스트2

--- src/main/java/com/spring/boot/MainApplication.java
+++ src/main/java/com/spring/boot/MainApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class MainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        System.out.println("jenkins test");
+        System.out.println("jenkins test2");
         new SpringApplicationBuilder(MainApplication.class)
                 .run(args);
     }
