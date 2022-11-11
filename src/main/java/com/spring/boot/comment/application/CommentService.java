8bd2342d6a22be640c0c0b47f13fdcf4c035dba7
dmswjd4117 <dmswjd4117@naver.com>
2022-11-12 07:34:17 +0900

    refactor: 본인이 작성한 글에 댓글달 수 있도록 수정

--- src/main/java/com/spring/boot/comment/application/CommentService.java
+++ src/main/java/com/spring/boot/comment/application/CommentService.java
@@ -15,31 +15,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommentService {
 
-    private final CommentRepository commentRepository;
-    private final PostRepository postRepository;
-    private final ConnectionService connectionService;
-    private final MemberService memberService;
+  private final CommentRepository commentRepository;
+  private final PostRepository postRepository;
+  private final ConnectionService connectionService;
+  private final MemberService memberService;
 
-    public CommentService(CommentRepository commentRepository, PostRepository postRepository, ConnectionService connectionService, MemberService memberService) {
-        this.commentRepository = commentRepository;
-        this.postRepository = postRepository;
-        this.connectionService = connectionService;
-        this.memberService = memberService;
-    }
+  public CommentService(CommentRepository commentRepository, PostRepository postRepository,
+      ConnectionService connectionService, MemberService memberService) {
+    this.commentRepository = commentRepository;
+    this.postRepository = postRepository;
+    this.connectionService = connectionService;
+    this.memberService = memberService;
+  }
 
-    @Transactional
-    public Comment createComment(Long writerId, String body, Long postId) {
-        Member commentWriter = memberService.findById(writerId)
-                .orElseThrow(()->new NotFoundException(Member.class, "id", writerId));
+  @Transactional
+  public Comment createComment(Long writerId, String body, Long postId) {
+    Member commentWriter = memberService.findById(writerId)
+        .orElseThrow(() -> new NotFoundException(Member.class, "id", writerId));
 
-        return postRepository.findById(postId)
-                .map(findPost -> {
-                    Member postWriter = findPost.getMember();
-                    if(!connectionService.checkMemberFollowsTargetMember(writerId, postWriter.getId())){
-                        throw new NotConnectedException(Member.class, writerId, "doesn't follow", postWriter.getId());
-                    }
-                    return commentRepository.save(new Comment(commentWriter, findPost, body));
-                })
-                .orElseThrow(()->new NotFoundException(Post.class, "id",postId));
-    }
+    return postRepository.findById(postId)
+        .map(findPost -> {
+          Member postWriter = findPost.getMember();
+          if (!postWriter.getId().equals(writerId) &&
+              !connectionService.checkMemberFollowsTargetMember(writerId, postWriter.getId())) {
+            throw new NotConnectedException(Member.class, writerId, "doesn't follow",
+                postWriter.getId());
+          }
+          return commentRepository.save(new Comment(commentWriter, findPost, body));
+        })
+        .orElseThrow(() -> new NotFoundException(Post.class, "id", postId));
+  }
 }
