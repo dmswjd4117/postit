b046fc654487f7a1b6a9dd57f6f2bab2cf97df62
dmswjd4117 <dmswjd4117@naver.com>
2022-11-16 12:52:29 +0900

    refactor: MethodArgumentNotValidException 예외 처리 핸들러 생성

--- src/main/java/com/spring/boot/common/error/GeneralExceptionHandler.java
+++ src/main/java/com/spring/boot/common/error/GeneralExceptionHandler.java
@@ -6,33 +6,40 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+  private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private ResponseEntity<ApiResult<?>> newResponse(Throwable throwable, HttpStatus httpStatus){
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        return new ResponseEntity<>(ApiResult.error(throwable, httpStatus), headers, httpStatus);
-    }
+  private ResponseEntity<ApiResult<?>> newResponse(Throwable throwable, HttpStatus httpStatus) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    return new ResponseEntity<>(ApiResult.error(throwable, httpStatus), headers, httpStatus);
+  }
 
-    @ExceptionHandler(ServiceRuntimeException.class)
-    private ResponseEntity<?> handleServiceRuntimeException(ServiceRuntimeException exception){
-        if(exception instanceof DuplicatedException){
-            return newResponse(exception, HttpStatus.BAD_REQUEST);
-        }
-        return newResponse(exception, HttpStatus.INTERNAL_SERVER_ERROR);
+  @ExceptionHandler(ServiceRuntimeException.class)
+  private ResponseEntity<?> handleServiceRuntimeException(ServiceRuntimeException exception) {
+    if (exception instanceof DuplicatedException) {
+      return newResponse(exception, HttpStatus.BAD_REQUEST);
     }
+    return newResponse(exception, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 
-    @ExceptionHandler({
-            IllegalArgumentException.class, IllegalStateException.class
-    })
-    private ResponseEntity<?> handleBadRequestException(RuntimeException exception){
-        log.debug("Bad request exception : {}", exception.getMessage());
-        return newResponse(exception, HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler({
+      IllegalArgumentException.class, IllegalStateException.class
+  })
+  private ResponseEntity<?> handleBadRequestException(RuntimeException exception) {
+    log.debug("Bad request exception : {}", exception.getMessage());
+    return newResponse(exception, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> handleValidationExceptions(
+      MethodArgumentNotValidException exception) {
+    return newResponse(exception, HttpStatus.BAD_REQUEST);
+  }
 }
