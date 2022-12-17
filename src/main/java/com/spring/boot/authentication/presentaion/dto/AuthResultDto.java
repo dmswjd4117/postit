85c0fd33750dfaa1b8a5c2b50bf128f0a56239df
dmswjd4117 <dmswjd4117@naver.com>
2022-12-17 13:35:19 +0900

    refactor: 디렉터리 삭제

--- src/main/java/com/spring/boot/authentication/presentaion/dto/AuthResultDto.java
+++
@@ -1,8 +0,0 @@
-package com.spring.boot.authentication.presentaion.dto;
-
-import com.spring.boot.member.presentaion.dto.MemberResponse;
-
-public class AuthResultDto {
-    private String apiToken;
-    private MemberResponse member;
-}
