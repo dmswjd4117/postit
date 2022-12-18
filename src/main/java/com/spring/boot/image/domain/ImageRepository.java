cddff32ee46f000428ba942777843fd2490e58ef
ten <dmswjd4117@naver.com>
2022-12-19 06:26:39 +0900

    refactor: ImageRepository 삭제

--- src/main/java/com/spring/boot/image/domain/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.boot.image.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<PostImage, Long> {
-
-}
