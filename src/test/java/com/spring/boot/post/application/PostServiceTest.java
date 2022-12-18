61002afecc97c07fd0eff45dda65da9100eca5e3
dmswjd4117 <dmswjd4117@naver.com>
2022-12-18 12:17:30 +0900

    refactor: post 수정 기능 테스트

--- src/test/java/com/spring/boot/post/application/PostServiceTest.java
+++ src/test/java/com/spring/boot/post/application/PostServiceTest.java
@@ -1,7 +1,7 @@
 package com.spring.boot.post.application;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -10,15 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 import com.spring.boot.common.config.InfrastructureTestConfiguration;
+import com.spring.boot.common.error.NotFoundException;
 import com.spring.boot.member.domain.member.Member;
 import com.spring.boot.member.domain.member.MemberRepository;
 import com.spring.boot.post.domain.Post;
 import com.spring.boot.post.domain.PostRepository;
 import com.spring.boot.post.domain.tag.PostTag;
-import com.spring.boot.post.presentaion.dto.PostRequest;
+import com.spring.boot.post.presentaion.dto.PostCreateRequest;
 import com.spring.boot.util.DatabaseCleanUp;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.RandomUtils;
@@ -61,16 +63,26 @@ class PostServiceTest {
   }
 
   Member createDummyMember() {
-    return memberRepository.save(new Member(RandomUtils.nextLong()+"@gmail.com", "pass", "name"));
+    return memberRepository.save(new Member(RandomUtils.nextLong() + "@gmail.com", "pass", "name"));
   }
 
-  PostRequest createPostRequestDto() {
-    return new PostRequest(TITLE, BODY, TAG_NAMES);
+  PostCreateRequest createPostRequestDto() {
+    return new PostCreateRequest(TITLE, BODY, TAG_NAMES);
   }
 
   Post createDummyPost(Long memberId) {
-    PostRequest postRequest = createPostRequestDto();
-    return postService.createPost(memberId, postRequest, IMAGES);
+    PostCreateRequest postCreateRequest = createPostRequestDto();
+    return postService.createPost(memberId, postCreateRequest, IMAGES);
+  }
+
+  private void assertTags(Post post, List<String> tagNames) {
+    assertAll(() -> {
+      assertThat(post.getPostTags().size(), is(tagNames.size()));
+      assertThat(post.getPostTags()
+          .stream()
+          .map(PostTag::getTagName)
+          .collect(Collectors.toSet()), is(equalTo(new HashSet<>(tagNames))));
+    });
   }
 
   @Test
@@ -78,16 +90,16 @@ class PostServiceTest {
   void 포스트_생성() {
     // given
     Member member = createDummyMember();
-    PostRequest postRequest = new PostRequest("title", "body", TAG_NAMES);
+    PostCreateRequest postCreateRequest = new PostCreateRequest("title", "body", TAG_NAMES);
 
     // when
-    Post createdPost = postService.createPost(member.getId(), postRequest, IMAGES);
+    Post createdPost = postService.createPost(member.getId(), postCreateRequest, IMAGES);
 
     // then
     assertNotNull(createdPost);
-    assertThat(createdPost.getBody(), is(postRequest.getBody()));
-    assertThat(createdPost.getTitle(), is(postRequest.getTitle()));
-    assertThat(createdPost.getPostTags().size(), is(postRequest.getTagNames().size()));
+    assertThat(createdPost.getContent(), is(postCreateRequest.getBody()));
+    assertThat(createdPost.getTitle(), is(postCreateRequest.getTitle()));
+    assertThat(createdPost.getPostTags().size(), is(postCreateRequest.getTagNames().size()));
   }
 
   @Test
@@ -95,8 +107,8 @@ class PostServiceTest {
   void 포스트_삭제() {
     // given
     Member member = createDummyMember();
-    PostRequest postRequest = new PostRequest("title", "body", TAG_NAMES);
-    Post createdPost = postService.createPost(member.getId(), postRequest, IMAGES);
+    PostCreateRequest postCreateRequest = new PostCreateRequest("title", "body", TAG_NAMES);
+    Post createdPost = postService.createPost(member.getId(), postCreateRequest, IMAGES);
 
     // when
     postService.deletePost(member.getId(), createdPost.getId());
@@ -108,22 +120,46 @@ class PostServiceTest {
     // given
     Member writer = createDummyMember();
     Member member = createDummyMember();
-    PostRequest postRequest = new PostRequest("title", "body", TAG_NAMES);
-    Post createdPost = postService.createPost(writer.getId(), postRequest, IMAGES);
+    PostCreateRequest postCreateRequest = new PostCreateRequest("title", "body", TAG_NAMES);
+    Post createdPost = postService.createPost(writer.getId(), postCreateRequest, IMAGES);
 
     // when
-    assertThrows(AccessDeniedException.class, ()->{
+    assertThrows(AccessDeniedException.class, () -> {
       postService.deletePost(member.getId(), createdPost.getId());
     });
   }
 
   @Test
+  @DisplayName("글 제목, 내용, 태그들을 수정할 수 있다.")
+  void 포스트_수정() {
+    // given
+    String NEW_TITLE = "new title";
+    String NEW_CONTENT = "new content";
+    List<String> NEW_TAG_NAMES = Arrays.asList("tag1", "newa", "newb");
+
+    Member writer = createDummyMember();
+    PostCreateRequest postCreateRequest = new PostCreateRequest("title", "body", TAG_NAMES);
+    Post createdPost = postService.createPost(writer.getId(), postCreateRequest, IMAGES);
+
+    // when
+    postService.updatePost(writer.getId(), createdPost.getId(), NEW_TITLE, NEW_CONTENT,
+        NEW_TAG_NAMES);
+    Post updated = postService.getPostByPostId(createdPost.getId());
+
+    // then
+    assertThat(updated.getTitle(), is(NEW_TITLE));
+    assertThat(updated.getContent(), is(NEW_CONTENT));
+    assertTags(updated, NEW_TAG_NAMES);
+
+  }
+
+  @Test
   @DisplayName("잘못된 유저 아이디일 경우 생성할 수 없다.")
   void 포스트_생성_실패_잘못된_유저아이디() {
-    PostRequest postRequest = new PostRequest("title", "body", Collections.emptyList());
+    PostCreateRequest postCreateRequest = new PostCreateRequest("title", "body", Collections.emptyList());
 
-    assertThrows(IllegalArgumentException.class, () -> {
-      postService.createPost(-1L, postRequest, Collections.emptyList());
+    assertThrows(NotFoundException.class, () -> {
+      postService.createPost(-1L, postCreateRequest, Collections.emptyList());
     });
   }
 
@@ -140,10 +176,10 @@ class PostServiceTest {
     // then
     assertThat(findPost, is(notNullValue()));
 
-    assertThat(findPost.getBody(), is(BODY));
+    assertThat(findPost.getContent(), is(BODY));
     assertThat(findPost.getMember().getName(), is(member.getName()));
     assertThat(findPost.getPostImages().size(), is(IMAGES.size()));
-    assertTags(findPost);
+    assertTags(findPost, TAG_NAMES);
   }
 
 
@@ -167,21 +203,11 @@ class PostServiceTest {
       Post findPost = posts.get(index);
       assertThat(findPost, is(notNullValue()));
 
-      assertThat(findPost.getBody(), is(BODY));
+      assertThat(findPost.getContent(), is(BODY));
       assertThat(findPost.getMember().getName(), is(member.getName()));
       assertThat(findPost.getPostImages().size(), is(IMAGES.size()));
-      assertTags(findPost);
+      assertTags(findPost, TAG_NAMES);
     }
-  }
-
-  private void assertTags(Post post) {
-    assertAll(() -> {
-      assertThat(post.getPostTags().size(), is(TAG_NAMES.size()));
-      assertThat(post.getPostTags()
-          .stream()
-          .map(PostTag::getTagName)
-          .collect(Collectors.toList()), containsInAnyOrder("tag1", "tag2"));
-    });
   }
 
 }
