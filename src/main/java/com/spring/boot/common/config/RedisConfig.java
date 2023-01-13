6d6ca4579ddb15456da9358508b723259d2ae290
dmswjd4117 <dmswjd4117@naver.com>
2023-01-14 05:21:35 +0900

    feat: 세션저장소 메모리 -> 레디스로 변경

---
+++ src/main/java/com/spring/boot/common/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.spring.boot.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+
+  @Value("${spring.redis.session.host}")
+  private String HOST;
+
+  @Value("${spring.redis.session.port}")
+  private int PORT;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(HOST, PORT);
+  }
+
+  @Bean
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    return redisTemplate;
+  }
+
+}
